ec0449c41424c0c462ddc883a8a3ff817b0358db
hailianguniontech <mahailiang@uniontech.com>
2025-09-16 11:31:08 +0800

    定义与 IPMI 协议规范直接相关的核心常量&write ipmi_oem.rs

---
+++ src/ipmi/ipmi_oem.rs
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+// struct ipmi_oem_handle {
+// 	const char * name;
+// 	const char * desc;
+// 	int (*setup)(struct ipmi_intf * intf);
+// };
